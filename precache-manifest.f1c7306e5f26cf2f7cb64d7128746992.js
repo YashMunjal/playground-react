cf87deff84f2491a2acb1002687f86ff478dd7d3
yashmunjal <yashmu45@gmail.com>
2020-09-20 16:05:26 +0530

    Updates

---
+++ precache-manifest.f1c7306e5f26cf2f7cb64d7128746992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5788677d92b807a808eb340d274ec2",
+    "url": "/playground-react/index.html"
+  },
+  {
+    "revision": "ad89ce3d7ab824adc9b4",
+    "url": "/playground-react/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "4679a3c38f6e831b38ce",
+    "url": "/playground-react/static/css/main.fb125279.chunk.css"
+  },
+  {
+    "revision": "ad89ce3d7ab824adc9b4",
+    "url": "/playground-react/static/js/2.d167ecae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playground-react/static/js/2.d167ecae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4679a3c38f6e831b38ce",
+    "url": "/playground-react/static/js/main.cefc82fd.chunk.js"
+  },
+  {
+    "revision": "f987dbf5cfd40e00d3cc",
+    "url": "/playground-react/static/js/runtime-main.dabacdfd.js"
+  }
+]);
